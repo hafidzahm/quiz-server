a8e91ee10a46fb08579614bd8f5e03155d7d0487
hafidzahm <hafizhahm123@gmail.com>
2025-05-24 11:55:52 +0700

    chore: configuring cors and database_url from env

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const QuestionController = require("./controllers/QuestionController");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3000;
 
+app.use(cors({ origin: "*" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
